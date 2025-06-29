f7172b47b9cafe51af4f802dddf3afa59f8ef2a7
hieudnguyen <hieund898@gmail.com>
2025-06-29 09:46:27 +0700

    read write file

---
+++ src/main/java/file_io/FileReadWrite.java
@@ -0,0 +1,31 @@
+package file_io;
+
+import java.io.*;
+
+public class FileReadWrite {
+
+    public static void main(String[] args) {
+        
+        String filePath = "output.text";
+        String content = "This is content which was written into the file.\n The second lie.";
+
+        //Write file
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(content);
+            System.out.println("Write file successfully");
+        } catch (IOException e) {
+            System.out.println("Error when write file " + e.getMessage());
+        }
+
+        //Read file
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            System.out.println("Read content from file");
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(">> " + line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error when read file " + e.getMessage());
+        }
+    }
+}
