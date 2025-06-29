5bbcd80314e6c403b782a435d65bf765ef87f996
hieudnguyen <hieund898@gmail.com>
2025-06-29 09:18:31 +0700

    string pratices

--- src/main/java/string_collection/StringPractice.java
+++ src/main/java/string_collection/StringPractice.java
@@ -1,8 +1,35 @@
 package string_collection;
 
+import java.util.Scanner;
+
 public class StringPractice {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Type a string: ");
+        String input = scanner.nextLine();
+
+        //Count the words
+        String[] words = input.trim().split("\\s+");
+        System.out.println("The counter of words in a string: " + words.length);
+
+        //Print uppercase
+        System.out.println("Uppercase the string: " + input.toUpperCase());
+
+        //Reverse string
+        String reversed = new StringBuilder(input).reverse().toString();
+        System.out.println("Reversed string: " + reversed);
+
+        //Palindrome checking
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        String reversedNormalized = new StringBuilder(normalized).reverse().toString();
+        if (normalized.equals(reversedNormalized)) {
+            System.out.println("This is a palindrome string");
+        } else {
+            System.out.println("This is not a palindrome string");
+        }
+
+        scanner.close();
     }
 }
